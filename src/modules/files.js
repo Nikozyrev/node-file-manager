b6f45876e8c46b628b82479e640e072ea769a451
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-06-23 12:33:51 +0300

    feat: complete files module

--- src/modules/files.js
+++ src/modules/files.js
@@ -1,5 +1,7 @@
-import { resolve } from 'path';
-import { createReadStream } from 'fs';
+import { basename, dirname, join, resolve } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { open, rename, unlink, writeFile } from 'fs/promises';
+import { pipeline } from 'stream/promises';
 import { userCommands } from '../controller/commands.js';
 import { FailedOperationError, InvalidInputError } from '../utils/errors.js';
 
@@ -12,9 +14,7 @@ export class FilesModule {
   }
 
   #readFile(path) {
-    if (!path) throw new InvalidInputError();
-    const currentDir = this.#controller.currentDir;
-    const pathToFile = resolve(currentDir, path);
+    const pathToFile = this.#getFullPath(path);
     const stream = createReadStream(pathToFile);
     stream.on('data', (chunk) => console.log(chunk.toString()));
     return new Promise((resolve, reject) => {
@@ -23,9 +23,81 @@ export class FilesModule {
     });
   }
 
+  async #createFile(path) {
+    const pathToFile = this.#getFullPath(path);
+    try {
+      await writeFile(pathToFile, '', { flag: 'wx' });
+    } catch {
+      throw new FailedOperationError();
+    }
+  }
+
+  async #renameFile(path, newFilename) {
+    if (!(path && newFilename)) throw new InvalidInputError();
+    const pathToFile = this.#getFullPath(path);
+    const newPath = join(dirname(pathToFile), newFilename);
+    try {
+      await rename(pathToFile, newPath);
+    } catch {
+      throw new FailedOperationError();
+    }
+  }
+
+  async #copyFile(path, destination) {
+    if (!(path && destination)) throw new InvalidInputError();
+    const pathToFile = this.#getFullPath(path);
+    const fileName = basename(pathToFile);
+    const destPath = this.#getFullPath(join(destination, fileName));
+
+    try {
+      const fd = await open(pathToFile);
+      const readStream = fd.createReadStream();
+      const writeStream = createWriteStream(destPath, { flags: 'wx' });
+      return await pipeline(readStream, writeStream);
+    } catch {
+      throw new FailedOperationError();
+    }
+  }
+
+  async #moveFile(path, destination) {
+    await this.#copyFile(path, destination);
+    await this.#deleteFile(path);
+  }
+
+  async #deleteFile(path) {
+    const pathToFile = this.#getFullPath(path);
+    try {
+      await unlink(pathToFile);
+    } catch {
+      throw new FailedOperationError();
+    }
+  }
+
   #subscribe() {
     this.#controller.subscribe(userCommands.cat, (...args) =>
       this.#readFile(...args)
     );
+    this.#controller.subscribe(userCommands.add, (...args) =>
+      this.#createFile(...args)
+    );
+    this.#controller.subscribe(userCommands.rn, (...args) =>
+      this.#renameFile(...args)
+    );
+    this.#controller.subscribe(userCommands.cp, (...args) =>
+      this.#copyFile(...args)
+    );
+    this.#controller.subscribe(userCommands.mv, (...args) =>
+      this.#moveFile(...args)
+    );
+    this.#controller.subscribe(userCommands.rm, (...args) =>
+      this.#deleteFile(...args)
+    );
+  }
+
+  #getFullPath(path) {
+    if (!path) throw new InvalidInputError();
+    const currentDir = this.#controller.currentDir;
+    const pathToFile = resolve(currentDir, path);
+    return pathToFile;
   }
 }
